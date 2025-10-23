83cc499945b740cbf5735e83bc806b51945acb74
JaxChoong <choongjax@gmail.com>
2025-10-23 14:28:41 +0800

    feat: now censors guessed word

--- hangman.rb
+++ hangman.rb
@@ -1,9 +1,20 @@
-words = []
-File.open("google-10000-english-no-swears.txt", "r") do |file|
-  file.each_line do |line|
-    words << line if line.chomp.length.between?(5, 12)
+require_relative "lib/game_state"
+require "yaml"
+
+# load saved game
+saved_game = YAML.load_file("game_state.yaml")
+
+# if no saved game, create new game
+unless saved_game
+  words = []
+  File.open("google-10000-english-no-swears.txt", "r") do |file|
+    file.each_line do |line|
+      words << line if line.chomp.length.between?(5, 12)
+    end
   end
+  secret_word = words[rand(words.length)].chomp
+  guessed = secret_word.gsub(/[a-z]/i, "_")
 end
 
-secret_word = words[rand(words.length)].chomp
 p secret_word
+p guessed
---
+++ lib/game_state.rb
@@ -0,0 +1,8 @@
+class Game_state
+  attr_accessor :secret_word, :guessed, :mistake_count
+  def initialize(secret_word,guessed,mistake_count=0)
+    @secret_word = word
+    @guessed = guessed
+    @mistake_count = mistake_count
+  end
+end
