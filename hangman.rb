87184c98ed02af70a38a2035dbbd0d5dbec53f2b
JaxChoong <choongjax@gmail.com>
2025-10-23 14:34:39 +0800

    chore: refactored load_game function

--- hangman.rb
+++ hangman.rb
@@ -1,23 +1,26 @@
 require_relative "lib/game_state"
 require "yaml"
 
-# load saved game
-saved_game = YAML.load_file("game_state.yaml")
+def load_game
+  # load saved game
+  saved_game = YAML.load_file("game_state.yaml")
 
-# if no saved game, create new game
-unless saved_game
-  words = []
-  File.open("google-10000-english-no-swears.txt", "r") do |file|
-    file.each_line do |line|
-      words << line if line.chomp.length.between?(5, 12)
+  # if no saved game, create new game
+  unless saved_game
+    words = []
+    File.open("google-10000-english-no-swears.txt", "r") do |file|
+      file.each_line do |line|
+        words << line if line.chomp.length.between?(5, 12)
+      end
     end
+    secret_word = words[rand(words.length)].chomp
+    guessed = secret_word.gsub(/[a-z]/i, "_")
+    current_state = Game_state.new(secret_word, guessed)
+  else
+    current_state = Game_state.new(saved_game[:secret_word], saved_game[:guessed], saved_game[:mistake_count])
   end
-  secret_word = words[rand(words.length)].chomp
-  guessed = secret_word.gsub(/[a-z]/i, "_")
-  current_state = Game_state.new(secret_word, guessed)
-else
-  current_state = Game_state.new(saved_game[:secret_word], saved_game[:guessed], saved_game[:mistake_count])
 end
 
+current_state = load_game
 p current_state.secret_word
 p current_state.guessed
